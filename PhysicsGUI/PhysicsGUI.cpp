028c6c2bb9bcf29e92a47fa762aebdf1cf13c406
MathiasDegeorges74 <mathias.degeorges@orange.fr>
2025-03-01 12:58:10 +0100

    Add script from SFML POC
    Stabilise FPS
    
    add font file

--- PhysicsGUI/PhysicsGUI.cpp
+++ PhysicsGUI/PhysicsGUI.cpp
@@ -1,24 +1,101 @@
 #include <SFML/Graphics.hpp>
+#include <format>
+#include <cmath>
+#include <numbers>
+#include <iostream>
 #include "Solution.hpp"
 #include "System.hpp"
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 1080, 720 }), "SFML works!");
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	double fpsTarget = 62.0;
+	double frameTime = 1.0 / fpsTarget;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	//window.setFramerateLimit(62);
+
+	sf::CircleShape circleExact(20.f);
+	sf::CircleShape circleEuler(20.f);
+	sf::CircleShape circleRK4(20.f);
+
+	circleExact.setFillColor(sf::Color(255, 0, 0, 255));
+	circleEuler.setFillColor(sf::Color(0, 255, 0, 255));
+	circleRK4.setFillColor(sf::Color(0, 0, 255, 255));
+
+	sf::Font font("arial.ttf"); // Throws sf::Exception if an error occurs
+
+	sf::Text text(font);
+	text.setString("hello");
+
+	sf::Text textExact(font);
+	textExact.setString("Exact");
+
+	sf::Text textEuler(font);
+	textEuler.setString("Euler");
+
+	sf::Text textRK4(font);
+	textRK4.setString("RK4");
+
+	sf::Clock clockGeneral;
+	sf::Clock clockFrame;
+
+	float yExact(0.0), yEuler(0.0), yRK4(0.0);
+
+	sf::Time timeSinceLastFrame = clockFrame.restart();
+	sf::Time elapsedTime = clockGeneral.getElapsedTime();
+	sf::Time frameElapsedTime = clockFrame.getElapsedTime();
+
+	while (window.isOpen())
+	{
+		timeSinceLastFrame = clockFrame.restart();
+		elapsedTime = clockGeneral.getElapsedTime();
+		//std::cout << std::format("time error (micros s) : {0:.5f}\n", ((1.0 / 62.0) - timeSinceLastFrame.asSeconds()) * 1000.0);
+
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		//    //text.setString(std::format("Time since a little butt is watching this : {0:.5f}", elapsedTime.asSeconds()));
+		text.setString(std::format("FPS : {0:.5f}", 1.0 / timeSinceLastFrame.asSeconds()));
+		text.setPosition(sf::Vector2f(100, 100));
+
+		yExact = sin(0.3 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
+		yEuler = sin(0.4 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
+		yRK4 = sin(0.5 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
+
+
+		circleExact.setPosition(sf::Vector2f(200.0 + 100.0 * yExact, 250.0));
+		circleEuler.setPosition(sf::Vector2f(200.0 + 100.0 * yEuler, 350.0));
+		circleRK4.setPosition(sf::Vector2f(200.0 + 100.0 * yRK4, 450.0));
+
+
+		textExact.setPosition(sf::Vector2f(200.0 + 100.0 * yExact, 200.0));
+		textEuler.setPosition(sf::Vector2f(200.0 + 100.0 * yEuler, 300.0));
+		textRK4.setPosition(sf::Vector2f(200.0 + 100.0 * yRK4, 400.0));
+
+
+		window.clear();
+		window.draw(text);
+
+		window.draw(circleExact);
+		window.draw(circleEuler);
+		window.draw(circleRK4);
+
+		window.draw(textExact);
+		window.draw(textEuler);
+		window.draw(textRK4);
+
+		window.display();
+
+		frameElapsedTime = clockFrame.getElapsedTime();
+		while (frameElapsedTime.asSeconds() < frameTime)
+		{
+			frameElapsedTime = clockFrame.getElapsedTime();
+			//std::cout << std::format("FET (micros s) : {0:.5f} \n", frameElapsedTime.asSeconds() * 1000.0);
+		}
+	}
 }
