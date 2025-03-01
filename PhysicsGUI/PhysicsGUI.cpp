e2f946e98a9ed717ec9184af8278253214681489
MathiasDegeorges74 <mathias.degeorges@orange.fr>
2025-03-01 14:54:40 +0100

    print frame overflow
    clean dummys body simulation

--- PhysicsGUI/PhysicsGUI.cpp
+++ PhysicsGUI/PhysicsGUI.cpp
@@ -9,49 +9,80 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({ 1080, 720 }), "SFML works!");
-
-	double fpsTarget = 62.0;
-	double frameTime = 1.0 / fpsTarget;
 
 	//window.setFramerateLimit(62);
 
-	sf::CircleShape circleExact(20.f);
-	sf::CircleShape circleEuler(20.f);
-	sf::CircleShape circleRK4(20.f);
+	sf::RenderWindow window(sf::VideoMode({ 1080, 720 }), "SFML works!");
 
-	circleExact.setFillColor(sf::Color(255, 0, 0, 255));
-	circleEuler.setFillColor(sf::Color(0, 255, 0, 255));
-	circleRK4.setFillColor(sf::Color(0, 0, 255, 255));
+	// Created simulation body objects
+
+
+	sf::CircleShape circleExact(20.f);
+	sf::CircleShape circleRK4(20.f);
+	sf::CircleShape circleEuler(20.f);
+
+	circleExact.setFillColor(sf::Color(255, 255, 255, 255));
+	circleRK4.setFillColor(sf::Color(0, 255, 0, 255));
+	circleEuler.setFillColor(sf::Color(255, 255, 0, 255));
 
 	sf::Font font("arial.ttf"); // Throws sf::Exception if an error occurs
 
-	sf::Text text(font);
-	text.setString("hello");
+	// HUD text
+	sf::Text textFPS(font);
+	sf::Text textOF(font);
+	sf::Text textOFMax(font);
 
+	textFPS.setString("FPS");
+	textOF.setString("OF");
+	textOFMax.setString("OFMax");
+
+	textFPS.setPosition(sf::Vector2f(100, 100));
+	textOF.setPosition(sf::Vector2f(500, 100));
+	textOFMax.setPosition(sf::Vector2f(500, 200));
+
+	// Body legends
 	sf::Text textExact(font);
-	textExact.setString("Exact");
-
+	sf::Text textRK4(font);
 	sf::Text textEuler(font);
+
+	textExact.setString("Exact");
+	textRK4.setString("RK4");
 	textEuler.setString("Euler");
 
-	sf::Text textRK4(font);
-	textRK4.setString("RK4");
+	textExact.setPosition(sf::Vector2f(200.0 + 100.0, 200.0));
+	textRK4.setPosition(sf::Vector2f(200.0 + 100.0, 300.0));
+	textEuler.setPosition(sf::Vector2f(200.0 + 100.0, 400.0));
 
-	sf::Clock clockGeneral;
-	sf::Clock clockFrame;
+	textExact.setFillColor(sf::Color(255, 255, 255, 255));
+	textRK4.setFillColor(sf::Color(0, 255, 0, 255));
+	textEuler.setFillColor(sf::Color(255, 255, 0, 255));
 
 	float yExact(0.0), yEuler(0.0), yRK4(0.0);
 
-	sf::Time timeSinceLastFrame = clockFrame.restart();
+	// Timing features 
+	sf::Clock clockGeneral; // General time clock
 	sf::Time elapsedTime = clockGeneral.getElapsedTime();
+
+	sf::Clock clockFrame; // Frame time clock
+	sf::Time timeSinceLastFrame = clockFrame.restart();
 	sf::Time frameElapsedTime = clockFrame.getElapsedTime();
+
+	double fpsTarget = 62.0;
+	double frameTime = 1.0 / fpsTarget;
+	double fps=0;
+
+	double frameOverFlow = 0; // Track frame time filling compared to target FPS
+	double frameOverFlowMax = 0; // History of peak frame time filling compared to target FPS
+
+
 
 	while (window.isOpen())
 	{
-		timeSinceLastFrame = clockFrame.restart();
 		elapsedTime = clockGeneral.getElapsedTime();
-		//std::cout << std::format("time error (micros s) : {0:.5f}\n", ((1.0 / 62.0) - timeSinceLastFrame.asSeconds()) * 1000.0);
+		// Check time as the begginig of the frame computing 
+		timeSinceLastFrame = clockFrame.restart();
+		fps = 1.0 / timeSinceLastFrame.asSeconds();
+		textFPS.setString(std::format("FPS : {0:.5f}", fps));
 
 		while (const std::optional event = window.pollEvent())
 		{
@@ -59,31 +90,24 @@ int main()
 				window.close();
 		}
 
-		//    //text.setString(std::format("Time since a little butt is watching this : {0:.5f}", elapsedTime.asSeconds()));
-		text.setString(std::format("FPS : {0:.5f}", 1.0 / timeSinceLastFrame.asSeconds()));
-		text.setPosition(sf::Vector2f(100, 100));
-
 		yExact = sin(0.3 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
-		yEuler = sin(0.4 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
 		yRK4 = sin(0.5 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
-
+		yEuler = sin(0.4 * elapsedTime.asSeconds() * 2 * std::numbers::pi + 0.5);
 
 		circleExact.setPosition(sf::Vector2f(200.0 + 100.0 * yExact, 250.0));
-		circleEuler.setPosition(sf::Vector2f(200.0 + 100.0 * yEuler, 350.0));
-		circleRK4.setPosition(sf::Vector2f(200.0 + 100.0 * yRK4, 450.0));
+		circleRK4.setPosition(sf::Vector2f(200.0 + 100.0 * yRK4, 350.0));
+		circleEuler.setPosition(sf::Vector2f(200.0 + 100.0 * yEuler, 450.0));
 
 
-		textExact.setPosition(sf::Vector2f(200.0 + 100.0 * yExact, 200.0));
-		textEuler.setPosition(sf::Vector2f(200.0 + 100.0 * yEuler, 300.0));
-		textRK4.setPosition(sf::Vector2f(200.0 + 100.0 * yRK4, 400.0));
-
-
+		// Draw all elements
 		window.clear();
-		window.draw(text);
+		window.draw(textFPS);
+		window.draw(textOF);
+		window.draw(textOFMax);
 
 		window.draw(circleExact);
-		window.draw(circleEuler);
 		window.draw(circleRK4);
+		window.draw(circleEuler);
 
 		window.draw(textExact);
 		window.draw(textEuler);
@@ -91,11 +115,26 @@ int main()
 
 		window.display();
 
+
+		// Check 
+		frameElapsedTime = clockFrame.getElapsedTime();
+		frameOverFlow = frameElapsedTime.asSeconds() / frameTime * 100.0;
+
+		textOF.setString(std::format("Frame filling [%] : {:07.2f}", frameOverFlow));
+
+		if ((frameOverFlow > frameOverFlowMax) && (elapsedTime.asSeconds() > 2))
+		{
+			frameOverFlowMax = frameOverFlow;
+			textOFMax.setString(std::format("Frame filling [%] : {:07.2f}", frameOverFlowMax));
+		}
+
+		// Stabilise the FPS by waiting before starting the next frame computing
 		frameElapsedTime = clockFrame.getElapsedTime();
 		while (frameElapsedTime.asSeconds() < frameTime)
 		{
 			frameElapsedTime = clockFrame.getElapsedTime();
 			//std::cout << std::format("FET (micros s) : {0:.5f} \n", frameElapsedTime.asSeconds() * 1000.0);
 		}
+
 	}
 }
